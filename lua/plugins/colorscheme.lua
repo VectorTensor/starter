4e32f7a4119dbc9f07a1919ccd68dbdb0b3b15e8
Prayash Thapa <prayash@lp-00337.ad.adex.ltd>
2025-03-12 05:21:04 +0000

    feat: add additional color schema

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,44 @@
+return {
+  -- Dracula Theme
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  -- Gruvbox Theme
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  -- TokyoNight Theme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "joshdick/onedark.vim", -- One Dark theme
+    lazy = false, -- Ensure it's loaded right away
+    priority = 1000, -- Load it with high priority
+  },
+  {
+    "EdenEast/nightfox.nvim", -- Noir theme under nightfox
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- Set Default Color Scheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon", -- Change this to "gruvbox" or "tokyonight" to switch
+    },
+  },
+}
