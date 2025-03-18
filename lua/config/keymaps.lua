13caf9d86d4dfab0842914f20955d4a6b0eacbdb
Prayash Thapa <prayash@lp-00337.ad.adex.ltd>
2025-03-18 10:08:43 +0000

    feat(debugger): add debugger and need to install dlv to run and also amd64 go compiler

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end)
+vim.keymap.set("n", "<F10>", function()
+  require("dap").step_over()
+end)
+vim.keymap.set("n", "<F11>", function()
+  require("dap").step_into()
+end)
+vim.keymap.set("n", "<F12>", function()
+  require("dap").step_out()
+end)
+vim.keymap.set("n", "<Leader>br", function()
+  require("dap").toggle_breakpoint()
+end)
+vim.keymap.set("n", "<Leader>Br", function()
+  require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end)
+
+vim.keymap.set("n", "<Leader>dT", function()
+  require("dap-go").debug_test()
+end)
+
+vim.keymap.set("n", "<Leader>dL", function()
+  require("dap-go").debug_last_test()
+end)
---
+++ lua/plugins/nvim-nio.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "nvim-neotest/nvim-nio",
+    config = function()
+      -- Configuration for nvim-nio (if any needed)
+    end,
+  },
+}
